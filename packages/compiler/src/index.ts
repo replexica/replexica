0cec835da1c9bd9ef0ecda3baedce3fdcab52521
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-03-09 00:57:06 -0800

    feat(compiler): add root export

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,6 +1,56 @@
-import { code, compile } from './code';
+import { createUnplugin } from "unplugin"
+import { ReplexicaAttributeScope, ReplexicaCompiler, ReplexicaContentScope, ReplexicaSkipScope } from "./compiler";
+import { ReplexicaOutputProcessor } from "./output";
+import path from 'path';
+import { createNextPlugin } from "./plugins";
+import { ReplexicaConfig } from "./types";
 
+const unplugin = createUnplugin<ReplexicaConfig>((options) => ({
+  name: '@replexica/compiler',
+  enforce: 'pre',
+  transformInclude(id) {
+    // .tsx and .jsx files
+    return /\.(t|j)sx$/.test(id);
+  },
+  transform(code, absoluteFilePath) {
+    try {
+      const relativeFilePath = path.relative(process.cwd(), absoluteFilePath);
+      
+      const relativeFileDir = path.relative(process.cwd(), path.dirname(absoluteFilePath));
+      const i18nImportPrefix = path.relative(relativeFileDir, options.i18nDir);
+
+      const compiler = ReplexicaCompiler
+        .fromCode(code, relativeFilePath, i18nImportPrefix)
+        .withScope(ReplexicaSkipScope)
+        .withScope(ReplexicaAttributeScope)
+        .withScope(ReplexicaContentScope)
+        .injectIntl();
+  
+      const result = compiler.generate();
+  
+      const outputProcessor = ReplexicaOutputProcessor.create(relativeFilePath, options);
+      outputProcessor.saveData(compiler.data);
+      outputProcessor.saveSourceLocaleData(compiler.data);
+  
+      if (options.debug) {
+        outputProcessor.saveAst(compiler.ast);
+        outputProcessor.saveOutput(result.code);
+      }
+  
+      return {
+        code: result.code,
+        map: result.map,
+      };
+    } catch (error: any) {
+      throw new ReplexicaError(error.message);
+    }
+  },
+}));
+
+class ReplexicaError extends Error {
+
+}
+export * from './types';
 export default {
-  code,
-  compile,
+  next: createNextPlugin(unplugin),
 };
