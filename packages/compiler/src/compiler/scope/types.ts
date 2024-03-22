f8f30418309d7b6746025bc27ce772026458e149
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-03-22 09:53:15 +0100

    feat(compiler): add `ReplexicaBaseScope` implementation

---
+++ packages/compiler/src/compiler/scope/types.ts
@@ -0,0 +1,13 @@
+import * as t from '@babel/types';
+import { NodePath } from '@babel/core';
+import { ReplexicaScopeData, ReplexicaScopeHint } from '../types';
+
+export interface IReplexicaScope {
+  get id(): string;
+  injectIntl(fileId: string, isServer: boolean, i18nImportPrefix: string): ReplexicaScopeData;
+  extractHints(): ReplexicaScopeHint[];
+}
+
+export type ReplexicaScopeExtractor = {
+  fromNode(path: NodePath<t.Node>): IReplexicaScope[];
+}
