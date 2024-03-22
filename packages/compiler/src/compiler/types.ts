e2ae80aa33dd94dbd44ecf086cf9d2ffc11ce81e
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-03-22 09:55:26 +0100

    feat(compiler): add `ReplexicaCompiler`

---
+++ packages/compiler/src/compiler/types.ts
@@ -0,0 +1,41 @@
+export type ReplexicaCompilerPayload = {
+  settings: {
+    locale: { source: string; };
+  };
+  data: ReplexicaCompilerData;
+};
+
+export type ReplexicaCompilerData = {
+  [fileId: string]: ReplexicaFilePayload;
+};
+
+export type ReplexicaFileContext = {
+  isClient: boolean;
+};
+
+export type ReplexicaFilePayload = {
+  context: ReplexicaFileContext;
+  data: ReplexicaFileData;
+};
+
+export type ReplexicaFileData = {
+  [scopeId: string]: ReplexicaScopePayload;
+}
+
+export type ReplexicaScopePayload = {
+  hints: ReplexicaScopeHint[];
+  data: ReplexicaScopeData;
+};
+
+export type ReplexicaScopeHint = {
+  type: 'attribute';
+  name: string;
+} | {
+  type: 'element';
+  name: string;
+  hint: string | null;
+};
+
+export type ReplexicaScopeData = {
+  [chunkId: string]: string;
+};
