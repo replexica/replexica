8055ac0360fa71761272602903cadb26089c9749
Max Prilutskiy <5614659+maxprilutskiy@users.noreply.github.com>
2024-03-23 15:12:57 +0100

    feat: add CLI project structure (#9)
    * feat(cli): add basic project structure
    
    * fix: cli commands location resolution
    
    * feat(cli): migrated to commander
    
    * chore: add empty changeset

---
+++ packages/cli/bin/cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+await import('../build/index.js');
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,18 @@
-export {};
+import { Command } from 'commander';
+import Ora from 'ora';
+import { setTimeout } from 'timers/promises';
+
+import loginCmd from './login.js';
+
+export default new Command()
+  .name('replexica')
+  .description('Replexica CLI')
+  .helpOption('-h, --help', 'Show help')
+  .addCommand(loginCmd)
+  .action(async (options) => {
+    const spinner = Ora();
+    spinner.start('Loading...');
+    await setTimeout(2000);
+    spinner.succeed('Welcome to Replexica CLI!');
+  })
+  .parse(process.argv);
