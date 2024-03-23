c5ce9298c2fbbe3176819178f9ef3aae42dbf241
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-03-23 14:41:33 +0100

    feat(cli): migrated to commander

--- packages/cli/src/commands/welcome.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from '@oclif/core'
-
-export default class Welcome extends Command {
-  static description = 'Replexica CLI';
-
-  async run(): Promise<void> {
-    this.log('Welcome to Replexica CLI');
-  }
-}
