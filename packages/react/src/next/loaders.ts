f3c43797d6a1028e9d9bc314095a845ab94ca215
Max Prilutskiy <5614659+maxprilutskiy@users.noreply.github.com>
2024-03-28 10:29:23 -0700

    docs: update readme (#11)
    * feat: readme
    
    * feat(react): update the name of the locale cookie
    
    * feat: upd readme
    
    * chore: add license

--- packages/react/src/next/loaders.ts
+++ packages/react/src/next/loaders.ts
@@ -4,5 +4,5 @@ import { cookies } from 'next/headers';
 
 export async function loadLocaleFromCookie() {
   const cookieMgr = cookies();
-  return cookieMgr.get('locale')?.value || 'en';
+  return cookieMgr.get('REPLEXICA_LOCALE')?.value || 'en';
 }
