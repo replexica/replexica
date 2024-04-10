afc83a86cba00aa766dc9ebbf3209a20ea13bdb9
Max Prilutskiy <5614659+maxprilutskiy@users.noreply.github.com>
2024-04-09 23:20:47 -0700

    feat(cli): add `replexica auth` command for easier cli <> api authentication (#42)
    * feat: add basic auth cmd structure
    
    * feat(cli): add authentication with ~/.replexicarc as storage
    
    * refactor(cli): move ini mgr into settings service
    
    * feat(cli): add `replexica auth` command implementation
    
    * chore: changeset

--- packages/cli/src/auth.ts
+++ packages/cli/src/auth.ts
@@ -1,17 +1,74 @@
 import { Command } from "commander";
+import Ora from 'ora';
+import express from 'express';
+import cors from 'cors';
+import open from 'open';
+import readline from 'readline/promises';
+import { loadSettings, saveSettings } from "./services/settings.js";
+import { getEnv } from "./services/env.js";
+import { checkAuth } from "./services/check-auth.js";
+import { saveApiKey } from "./services/api-key.js";
 
 export default new Command()
   .command("auth")
-  .description("Authenticate with Replexica")
+  .description("Authenticate with Replexica API")
   .helpOption("-h, --help", "Show help")
-  .action(async () => {
-    const message = `
-To obtain Replexica API key, please follow the following link:
+  .option("-d, --delete", "Delete existing authentication")
+  .option("-l, --login", "Authenticate with Replexica API")
+  .action(async (options) => {
+    const env = getEnv();
+    let config = await loadSettings();
 
-https://replexica.com/app/settings
+    if (options.delete) {
+      await logout();
+    }
+    if (options.login) {
+      await login(env.REPLEXICA_WEB_URL);
+      config = await loadSettings();
+    }
 
-Once you have your API key, please save it in the .env file in the root of your project.
-          `.trim();
-
-    console.log(message);
+    await checkAuth();
   });
+
+async function logout() {
+  const spinner = Ora().start('Logging out');
+  await saveApiKey(null);
+  spinner.succeed('Logged out');
+}
+
+async function login(apiUrl: string) {
+  await readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  }).question('Press Enter to open the browser for authentication\n');
+
+  const spinner = Ora().start('Waiting for the API key');
+  const apiKey = await waitForApiKey(async (port) => {
+    await open(`${apiUrl}/app/cli?port=${port}`, { wait: false });
+  });
+  spinner.succeed('API key received');
+  await saveApiKey(apiKey);
+}
+
+async function waitForApiKey(cb: (port: string) => void): Promise<string> {
+  // start a sever on an ephemeral port and return the port number
+  // from the function
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  return new Promise((resolve) => {
+    const server = app.listen(0, async () => {
+      const port = (server.address() as any).port;
+      cb(port.toString());
+    });
+
+    app.post("/", (req, res) => {
+      const apiKey = req.body.apiKey;
+      res.end();
+      server.close(() => {
+        resolve(apiKey);
+      });
+    });
+  });
+}
