9cb3bfcfbca9429c2abf125414571ab2d8455340
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-04-24 14:10:11 +0200

    feat(compiler): split metadata inference into phases

--- packages/compiler/src/plugins/base.ts
+++ packages/compiler/src/plugins/base.ts
@@ -4,44 +4,52 @@ import { ReplexicaOutputProcessor } from "./../output";
 import path from 'path';
 import { ReplexicaConfig, parseOptions } from "../options";
 
+export const shouldTransformFile = (absoluteFilePath: string) => /\.(t|j)sx$/.test(absoluteFilePath); // jsx, tsx
+
+export const transformFile = (code: string, absoluteFilePath: string, _options: Partial<ReplexicaConfig>) => {
+  const options = parseOptions(_options);
+  try {
+    const relativeFilePath = path.relative(process.cwd(), absoluteFilePath);
+
+    const compiler = ReplexicaCompiler
+      .fromCode(code, relativeFilePath, options.rsc)
+      .withScope(ReplexicaSkipScope)
+      .withScope(ReplexicaAttributeScope)
+      .withScope(ReplexicaContentScope)
+      .injectIntl();
+
+    const result = compiler.generate();
+
+    const outputProcessor = ReplexicaOutputProcessor.create(relativeFilePath, options);
+    outputProcessor.saveBuildData(compiler.data);
+    outputProcessor.saveFullSourceLocaleData(compiler.data);
+    outputProcessor.saveClientSourceLocaleData(compiler.data);
+    outputProcessor.saveStubLocaleData();
+
+    if (options.debug) {
+      outputProcessor.saveAst(compiler.ast);
+      outputProcessor.saveOutput(result.code);
+    }
+
+    return {
+      code: result.code,
+      map: result.map,
+    };
+  } catch (error: any) {
+    throw new ReplexicaError(error.message);
+  }
+};
+
 export const basePlugin = createUnplugin<Partial<ReplexicaConfig>>((_options) => ({
   name: '@replexica/compiler',
   enforce: 'pre',
   transformInclude(id) {
-    return /\.(t|j)sx?$/.test(id); // js, ts, jsx, tsx
+    const result = shouldTransformFile(id);
+    return result;
   },
   transform(code, absoluteFilePath) {
-    const options = parseOptions(_options);
-    try {
-      const relativeFilePath = path.relative(process.cwd(), absoluteFilePath);
-
-      const compiler = ReplexicaCompiler
-        .fromCode(code, relativeFilePath, options.rsc)
-        .withScope(ReplexicaSkipScope)
-        .withScope(ReplexicaAttributeScope)
-        .withScope(ReplexicaContentScope)
-        .injectIntl();
-  
-      const result = compiler.generate();
-  
-      const outputProcessor = ReplexicaOutputProcessor.create(relativeFilePath, options);
-      outputProcessor.saveBuildData(compiler.data);
-      outputProcessor.saveFullSourceLocaleData(compiler.data);
-      outputProcessor.saveClientSourceLocaleData(compiler.data);
-      outputProcessor.saveStubLocaleData();
-  
-      if (options.debug) {
-        outputProcessor.saveAst(compiler.ast);
-        outputProcessor.saveOutput(result.code);
-      }
-  
-      return {
-        code: result.code,
-        map: result.map,
-      };
-    } catch (error: any) {
-      throw new ReplexicaError(error.message);
-    }
+    const result = transformFile(code, absoluteFilePath, _options);
+    return result;
   },
 }));
 
