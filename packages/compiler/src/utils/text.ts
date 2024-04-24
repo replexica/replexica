33324583618acaa74e39c41860eab4b4a2c5fe75
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-04-24 15:24:05 +0200

    fix(compiler): fix logic error in trimSafely

--- packages/compiler/src/utils/text.ts
+++ packages/compiler/src/utils/text.ts
@@ -1,23 +1,23 @@
 export function trimSafely(inputText: string) {
   let result = inputText;
 
-  const leadingNbspCount = (inputText.match(/^\u00A0+/) || [''])[0].length;
+  const leadingNbspCount = (result.match(/^\u00A0+/) || [''])[0].length;
   
   // Trim start
   let resultWithTrimmedStart = result.trimStart();
-  resultWithTrimmedStart = '\u00A0'.repeat(leadingNbspCount) + result;
+  resultWithTrimmedStart = '\u00A0'.repeat(leadingNbspCount) + resultWithTrimmedStart;
   if (resultWithTrimmedStart.length !== result.length) {
-    resultWithTrimmedStart = ' ' + result;
+    resultWithTrimmedStart = ' ' + resultWithTrimmedStart;
   }
   result = resultWithTrimmedStart;
 
-  const trailingNbspCount = (inputText.match(/\u00A0+$/) || [''])[0].length;
+  const trailingNbspCount = (result.match(/\u00A0+$/) || [''])[0].length;
 
   // Trim end
   let resultWithTrimmedEnd = result.trimEnd();
-  resultWithTrimmedEnd = result + '\u00A0'.repeat(trailingNbspCount);
+  resultWithTrimmedEnd = resultWithTrimmedEnd + '\u00A0'.repeat(trailingNbspCount);
   if (resultWithTrimmedEnd.length !== result.length) {
-    resultWithTrimmedEnd = result + ' ';
+    resultWithTrimmedEnd = resultWithTrimmedEnd + ' ';
   }
   result = resultWithTrimmedEnd;
 
