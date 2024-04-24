65397f950268f747ecb3afa0f93516f550ecced2
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-04-24 11:50:29 +0200

    chore(demo): add shadcn project demo

---
+++ demo/next-app-shadcnui/next.config.mjs
@@ -0,0 +1,18 @@
+import compiler from '@replexica/compiler';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+/** @type {import('@replexica/compiler').ReplexicaConfig} */
+const replexicaConfig = {
+  locale: {
+    source: 'en',
+    targets: ['es'],
+  },
+  debug: true,    
+};
+
+export default compiler.next(
+  replexicaConfig,
+  nextConfig,
+);
