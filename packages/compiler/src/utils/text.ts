970510d3f82863470a5d0158a476a4840ba78a46
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-04-24 15:37:48 +0200

    fix(compiler): fix nbsp handling in trimSafely

--- packages/compiler/src/utils/text.ts
+++ packages/compiler/src/utils/text.ts
@@ -1,25 +1,8 @@
-export function trimSafely(inputText: string) {
-  let result = inputText;
-
-  const leadingNbspCount = (result.match(/^\u00A0+/) || [''])[0].length;
-  
-  // Trim start
-  let resultWithTrimmedStart = result.trimStart();
-  resultWithTrimmedStart = '\u00A0'.repeat(leadingNbspCount) + resultWithTrimmedStart;
-  if (resultWithTrimmedStart.length !== result.length) {
-    resultWithTrimmedStart = ' ' + resultWithTrimmedStart;
-  }
-  result = resultWithTrimmedStart;
-
-  const trailingNbspCount = (result.match(/\u00A0+$/) || [''])[0].length;
-
-  // Trim end
-  let resultWithTrimmedEnd = result.trimEnd();
-  resultWithTrimmedEnd = resultWithTrimmedEnd + '\u00A0'.repeat(trailingNbspCount);
-  if (resultWithTrimmedEnd.length !== result.length) {
-    resultWithTrimmedEnd = resultWithTrimmedEnd + ' ';
-  }
-  result = resultWithTrimmedEnd;
+export function trimSafely(input: string): string {
+  // repleace \n with space
+  let result = input.replace(/\n/g, ' ');
+  // remove duplicate spaces, but not the NBSPs
+  result = result.replace(/ +/g, ' ');
 
   return result;
 }
