d6d9bd464a09c101f5f4c144334833508c0c266a
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-05-02 18:56:05 +0200

    feat(compiler): use config merging in compiler

--- packages/compiler/src/plugins/next.ts
+++ packages/compiler/src/plugins/next.ts
@@ -1,33 +1,53 @@
 import walk from 'ignore-walk';
 import fs from 'fs';
+import _ from 'lodash';
 import path from 'path';
 import { basePlugin, shouldTransformFile, transformFile } from './base';
-import { ReplexicaConfig } from '../options';
+import { configFileSchema, defaultConfig } from '@replexica/spec';
 
-export function nextPlugin<O extends ReplexicaConfig>(replexicaOptions: O, nextConfig: any) {
-  return {
-    ...nextConfig,
-    webpack(config: any, ctx: any) {
-      const plugin = basePlugin.webpack(replexicaOptions);
-      config.plugins.unshift(plugin);
+export function nextPlugin(partialReplexicaConfig?: Partial<typeof configFileSchema._type>) {
+  const replexicaFileConfig = loadReplexicaFileConfig();
+  const finalReplexicaConfig = _.merge(
+    {},
+    defaultConfig,
+    replexicaFileConfig,
+    partialReplexicaConfig,
+  );
+  return (nextConfig: any) => {
+    return {
+      ...nextConfig,
+      webpack(config: any, ctx: any) {
+        const plugin = basePlugin.webpack(finalReplexicaConfig);
+        config.plugins.unshift(plugin);
+  
+        const files = walk.sync({
+          path: ctx.dir,
+          ignoreFiles: ['.gitignore'],
+        })
+          .map((relFilePath) => path.resolve(ctx.dir, relFilePath))
+          .filter((file) => shouldTransformFile(file));
+  
+        for (const file of files) {
+          const code = fs.readFileSync(file, 'utf-8');
+          transformFile(code, file, finalReplexicaConfig);
+        }
+  
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, ctx);
+        }
+  
+        return config;
+      },
+    };
+  }
+}
 
-      const files = walk.sync({
-        path: ctx.dir,
-        ignoreFiles: ['.gitignore'],
-      })
-        .map((relFilePath) => path.resolve(ctx.dir, relFilePath))
-        .filter((file) => shouldTransformFile(file));
+function loadReplexicaFileConfig(): any {
+  const replexicaConfigPath = path.resolve(process.cwd(), 'i18n.json');
+  const fileExists = fs.existsSync(replexicaConfigPath);
+  if (!fileExists) { return undefined; }
 
-      for (const file of files) {
-        const code = fs.readFileSync(file, 'utf-8');
-        transformFile(code, file, replexicaOptions);
-      }
-
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, ctx);
-      }
-
-      return config;
-    },
-  };
+  const fileContent = fs.readFileSync(replexicaConfigPath, 'utf-8');
+  const replexicaFileConfig = JSON.parse(fileContent);
+  return replexicaFileConfig;
 }
