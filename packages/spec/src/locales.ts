fee7db5b074501536c492e00e05a5181e3a1e101
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-05-03 12:59:38 +0200

    refactor(spec): extracted core locales into separate func

--- packages/spec/src/locales.ts
+++ packages/spec/src/locales.ts
@@ -1,11 +1,13 @@
 import Z from 'zod';
 
+const coreLocales = ['en', 'es'] as const;
+
 // Source
-export const sourceLocales = ['en'] as const;
+export const sourceLocales = [...coreLocales] as const;
 
 export const sourceLocaleSchema = Z.enum(sourceLocales);
 
 // Target
-export const targetLocales = ['es'] as const;
+export const targetLocales = [...coreLocales] as const;
 
 export const targetLocaleSchema = Z.enum(targetLocales);
