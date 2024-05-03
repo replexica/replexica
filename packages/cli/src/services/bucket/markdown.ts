18b98a0946ef2460fe4fce932a16fb1e194a5c49
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-05-03 08:36:16 +0200

    fix(cli): fix markdown bucket processor (de-)serialization

--- packages/cli/src/services/bucket/markdown.ts
+++ packages/cli/src/services/bucket/markdown.ts
@@ -1,7 +1,6 @@
 import { IBucketProcessor } from "./core.js";
 import { BaseBucketProcessor } from "./base.js";
 import _ from 'lodash';
-import objectHash from 'object-hash';
 
 export class MarkdownBucketProcessor extends BaseBucketProcessor implements IBucketProcessor {
   protected override _validateBucketPath(bucketPath: string): void {
@@ -18,16 +17,10 @@ export class MarkdownBucketProcessor extends BaseBucketProcessor implements IBuc
   }
 
   protected override async _deserializeData(content: string): Promise<Record<string, any>> {
-    const result = _.chain(content)
-      .split('\n\n')
-      .map((content, index) => [objectHash({ content, index }), content])
-      .fromPairs()
-      .value();
-    
-    return result;
+    return { '': content };
   }
 
   protected override async _serializeDataContent(data: Record<string, any>): Promise<string> {
-    return _.values(data).join('\n\n');
+    return data[''];
   }
 }
