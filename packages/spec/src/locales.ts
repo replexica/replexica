a966eda764840265bfc8e9dfd7d4307c801633c0
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-05-03 13:02:15 +0200

    feat(spec): enable `fr`, `ca`, and `cs` in source-only mode

--- packages/spec/src/locales.ts
+++ packages/spec/src/locales.ts
@@ -1,13 +1,23 @@
 import Z from 'zod';
 
-const coreLocales = ['en', 'es'] as const;
+const coreLocales = [
+  'en', // English
+  'es', // Spanish
+  'fr', // French
+  'ca', // Catalan
+] as const;
 
 // Source
-export const sourceLocales = [...coreLocales] as const;
+export const sourceLocales = [
+  ...coreLocales,
+  'cs', // Czech
+] as const;
 
 export const sourceLocaleSchema = Z.enum(sourceLocales);
 
 // Target
-export const targetLocales = [...coreLocales] as const;
+export const targetLocales = [
+  ...coreLocales,
+] as const;
 
 export const targetLocaleSchema = Z.enum(targetLocales);
