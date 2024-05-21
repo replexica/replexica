2e1ff1fd5ae0248cc553d1df8a526267739f7800
VeranikaP <veranika.prilutskaya@gmail.com>
2024-05-21 13:13:01 +0200

    feat: added support for 15 new languages

--- packages/spec/src/locales.ts
+++ packages/spec/src/locales.ts
@@ -5,12 +5,28 @@ const coreLocales = [
   'es', // Spanish
   'fr', // French
   'ca', // Catalan
+  'ja', // Japanese
+  'de', // German
+  'pt', // Portuguese
+  'it', // Italian
+  'ru', // Russian
+  'uk', // Ukrainian
+  'hi', // Hindi
+  'zh', // Mandarin
+  'ko', // Korean
+  'tr', // Turkish 
+  'ar', // Arabic
+
 ] as const;
 
 // Source
 export const sourceLocales = [
   ...coreLocales,
   'cs', // Czech
+  'yue', // Cantonese
+  'pl', // Polish
+  'sk', // Slovak
+  'th', // Thai
 ] as const;
 
 export const sourceLocaleSchema = Z.enum(sourceLocales);
