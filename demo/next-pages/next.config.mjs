e77caf268d3ac44891def550e5c5d5ca1909d843
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-05-26 17:27:00 +0200

    fix: default route resolution for pages router

--- demo/next-pages/next.config.mjs
+++ demo/next-pages/next.config.mjs
@@ -4,8 +4,8 @@ import i18n from './i18n.mjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: i18n.params.supportedLocales,
-    defaultLocale: i18n.params.defaultLocale,
+    locales: [...i18n.params.supportedLocales, '$'],
+    defaultLocale: '$',
     localeDetection: false,
   },
 };
