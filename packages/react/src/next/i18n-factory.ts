13e85572b94178a44cbacf898fbdab92bdc43665
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-05-30 22:56:33 +0200

    feat: new i18n.json + loaders format

--- packages/react/src/next/i18n-factory.ts
+++ packages/react/src/next/i18n-factory.ts
@@ -1,28 +1,21 @@
 'use server';
 
-import { GetStaticPropsContext } from "next";
-import { loadLocale } from "./utils";
+import { loadLocaleFromCookie } from "./utils";
+import { I18nInstance } from "../shared";
 
 export type I18nFactoryParams<C> = {
   resolver: (ctx: C) => Promise<string | null>;
   locales: Record<string, () => Promise<any>>,
 };
 
-export const localeResolvers = {
-  cookie: async function (ctx: void) {
-    const locale = await loadLocale();
-    return locale;
-  },
-  staticProps: async function (ctx: GetStaticPropsContext) {
-    const locale = ctx.locale || null;
-    return locale;
-  },
-};
-
-export function createI18nLoader<C = string>(params: I18nFactoryParams<C>) {
-  return async function load(ctx: C) {
+function createI18nLoader<C>(params: I18nFactoryParams<C>) {
+  return async function load(ctx: C): Promise<I18nInstance> {
     const supportedLocales = Object.keys(params.locales);
-    const defaultLocale = supportedLocales[0]!;
+    if (supportedLocales.length === 0) {
+      throw new Error('No locale dictionaries provided.');
+    }
+
+    const defaultLocale = supportedLocales[0];
     const currentLocale = await params.resolver(ctx) || defaultLocale;
     const data = await params.locales[currentLocale]?.()
       .then((mod) => mod.default) || {};
@@ -36,31 +29,27 @@ export function createI18nLoader<C = string>(params: I18nFactoryParams<C>) {
   }
 }
 
-export async function loadI18nFromStaticProps(
-  ctx: GetStaticPropsContext,
-  locales: I18nFactoryParams<GetStaticPropsContext>['locales'],
+export async function loadI18nFromParam(
+  locale: string | undefined,
+  locales: Record<string, () => Promise<any>> = {},
 ) {
-  return createI18nLoader({
-    resolver: localeResolvers.staticProps,
+  const loader = createI18nLoader({
+    resolver: async (ctx: string | null) => ctx,
     locales,
-  })(ctx);
+  });
+
+  const i18n = await loader(locale || null);
+  return i18n;
 }
 
 export async function loadI18nFromCookie(
-  locales: I18nFactoryParams<void>['locales'],
+  locales: Record<string, () => Promise<any>> = {},
 ) {
-  return createI18nLoader({
-    resolver: localeResolvers.cookie,
+  const loader = createI18nLoader<void>({
+    resolver: () => loadLocaleFromCookie(),
     locales,
-  })();
-}
+  });
 
-export async function loadI18nFromString(
-  locale: string,
-  locales: I18nFactoryParams<string>['locales'],
-) {
-  return createI18nLoader({
-    resolver: async (str) => str,
-    locales,
-  })(locale);
+  const i18n = await loader();
+  return i18n;
 }
