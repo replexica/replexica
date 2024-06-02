55a1b93581e9efb91cea85a09b4fd4a53eb0dd7c
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-06-02 14:28:07 +0200

    feat: i18n dictionaries

--- demo/next-pages/src/pages/_app.tsx
+++ demo/next-pages/src/pages/_app.tsx
@@ -1,20 +1,13 @@
 'use client';
 
 import "@/styles/globals.css";
-import { I18nProvider } from "@replexica/react/client";
+import { I18nProvider } from "@replexica/react";
 import type { AppProps } from "next/app";
 
 export default function App(appProps: AppProps) {
   return (
-    <I18nProvider
-      params={{
-        currentLocale: appProps.router.locale!,
-        defaultLocale: appProps.router.defaultLocale!,
-        supportedLocales: appProps.router.locales!,
-      }}
-      data={appProps.pageProps.i18n?.data || {}}
-    >
-      <appProps.Component {...appProps.pageProps} />;
+    <I18nProvider i18n={appProps.pageProps.i18n}>
+      <appProps.Component {...appProps.pageProps} />
     </I18nProvider>
   );
 }
