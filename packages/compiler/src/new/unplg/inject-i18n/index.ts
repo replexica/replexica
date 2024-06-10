c82f2ebc9f177ea7f7027881c3e7b01a93e4dc3e
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-06-10 10:37:09 +0200

    feat: i18n injectors structure

--- packages/compiler/src/new/unplg/inject-i18n/index.ts
+++ packages/compiler/src/new/unplg/inject-i18n/index.ts
@@ -1,5 +1,13 @@
+import * as t from "@babel/types";
 import { I18nScope } from "../_types";
 
-export default function injectI18n(i18nTree: I18nScope) {
+export default function createI18nInjector(ast: t.File) {
+  return {
+    injectLoaders: () => {
 
+    },
+    injectFragments: (i18nTree: I18nScope) => {
+
+    },
+  };
 }
