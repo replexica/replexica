1555b79aef172fefebce792d743a8d716b98e4af
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-06-13 10:52:05 +0200

    fix: fields in package jsons

--- packages/compiler/src/unplg/iom/_const.ts
+++ packages/compiler/src/unplg/iom/_const.ts
@@ -1,5 +1,5 @@
-export const CLIENT_IMPORT_MODULE = '@replexica/react';
-export const NEXTJS_IMPORT_MODULE = '@replexica/react/next';
+export const CLIENT_IMPORT_MODULE = 'replexica/react';
+export const NEXTJS_IMPORT_MODULE = 'replexica/react-next';
 
 export const I18N_IMPORT_NAME = 'I18n';
 export const I18N_ACCESS_METHOD = 'fromRscContext';
--- packages/react/src/shared/proxy.tsx
+++ packages/react/src/shared/proxy.tsx
@@ -3,15 +3,18 @@ import { I18nBaseFragmentProps, resolveI18nValue } from "./fragment";
 
 export type I18nBaseProxyProps<P extends {}> = P & {
   data: any;
+  $: {
+    Component: string | React.ComponentType<P>;
+    attributes: Record<keyof P, any>;
+  };
   $$Component: string | React.ComponentType<P>;
-  $$Attributes: Record<keyof P, Omit<I18nBaseFragmentProps, 'data'>>;
 };
 
 export function I18nBaseProxy<P extends {}>(props: I18nBaseProxyProps<P>) {
-  const { $$Attributes, $$Component, data, ...originalProps } = props;
+  const { $, data, ...originalProps } = props;
   let propsPatch: Partial<P> = {};
 
-  for (const [key, value] of Object.entries($$Attributes || {})) {
+  for (const [key, value] of Object.entries($.attributes || {})) {
     const selector = value as Omit<I18nBaseFragmentProps, 'data'>;
     const result = resolveI18nValue(data, selector);
 
@@ -27,7 +30,7 @@ export function I18nBaseProxy<P extends {}>(props: I18nBaseProxyProps<P>) {
   } as any;
 
   return createElement(
-    $$Component,
+    $.Component,
     modifiedProps,
   );
 }
