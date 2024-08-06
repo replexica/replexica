d293f059e1bd9131d6d41ceffc713efa8d6fa598
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-08-06 13:13:09 +0200

    feat(cli): remove unused keys #144

---
+++ .changeset/dull-wombats-cross.md
@@ -0,0 +1,6 @@
+---
+"@replexica/cli": minor
+"replexica": minor
+---
+
+New feature: remove unused keys, whenever a key gets deleted in the source file (thanks @quentin-decre!)
--- packages/cli/src/cli/i18n.ts
+++ packages/cli/src/cli/i18n.ts
@@ -8,9 +8,9 @@ import { createBucketProcessor } from './../workers/bucket';
 import { targetLocaleSchema } from '@replexica/spec';
 import { createLockfileProcessor } from './../workers/lockfile';
 import { createAuthenticator } from './../workers/auth';
-import fs from 'fs';
-import path from 'path';
 import { ReplexicaEngine } from '@replexica/sdk';
+// import fs from 'fs';
+// import path from 'path';
 
 export default new Command()
   .command('i18n')
@@ -20,20 +20,20 @@ export default new Command()
   .option('--bucket <bucket>', 'Bucket to process')
   .option('--frozen', `Don't update the translations and fail if an update is needed`)
   .option('--force', 'Ignore lockfile and process all keys')
-  .action(async function(options) {
+  .action(async function (options) {
     const ora = Ora();
     try {
       ora.start('Loading Replexica configuration');
       const [
-        settings, 
-        i18nConfig, 
-        flags, 
+        settings,
+        i18nConfig,
+        flags,
       ] = await Promise.all([
         loadSettings(),
         loadConfig(),
         loadFlags(options),
       ]);
-  
+
       if (!i18nConfig) {
         throw new Error('i18n.json not found. Please run `replexica init` to initialize the project.');
       } else if (!i18nConfig.buckets || !Object.keys(i18nConfig.buckets).length) {
@@ -46,23 +46,23 @@ export default new Command()
         ora.succeed('Replexica configuration loaded');
       }
 
-      ora.start('Deleting redundant i18m files');
-      let i18nLocales = [i18nConfig.locale.source, ...i18nConfig.locale.targets]
-      for (const bucketPath of Object.keys(i18nConfig.buckets)) {
-        let directory = bucketPath.substring(0, bucketPath.lastIndexOf('/'));
-        let parentDir = path.join(process.cwd(), directory);
+      // ora.start('Deleting redundant i18n files');
+      // let i18nLocales = [i18nConfig.locale.source, ...i18nConfig.locale.targets]
+      // for (const bucketPath of Object.keys(i18nConfig.buckets)) {
+      //   let directory = bucketPath.substring(0, bucketPath.lastIndexOf('/'));
+      //   let parentDir = path.join(process.cwd(), directory);
 
-        fs.readdirSync(parentDir).forEach(file => {
-          let fileName = file.substring(file.lastIndexOf('/') + 1);
-          let locale = fileName.substring(0, fileName.indexOf('.'));
-          const exists = i18nLocales.includes(locale);
-          if (!exists) {
-            let dir = parentDir + "/" + file;
-            fs.unlinkSync(dir);
-          }
-        })
-      }
-      ora.succeed('Redundant i18n files deleted')
+      //   fs.readdirSync(parentDir).forEach(file => {
+      //     let fileName = file.substring(file.lastIndexOf('/') + 1);
+      //     let locale = fileName.substring(0, fileName.indexOf('.'));
+      //     const exists = i18nLocales.includes(locale);
+      //     if (!exists) {
+      //       let dir = parentDir + "/" + file;
+      //       fs.unlinkSync(dir);
+      //     }
+      //   })
+      // }
+      // ora.succeed('Redundant i18n files deleted')
 
       ora.start('Connecting to AI localization engine');
       const authenticator = createAuthenticator({
@@ -89,7 +89,7 @@ export default new Command()
       const targetedBuckets = flags.bucket ? { [flags.bucket]: i18nConfig.buckets[flags.bucket] } : i18nConfig.buckets;
       for (const [bucketPath, bucketType] of Object.entries(targetedBuckets)) {
         console.log('');
-        const bucketOra = Ora({ });
+        const bucketOra = Ora({});
         bucketOra.info(`Processing ${bucketPath}`);
         // Create the payload processor instance for the current bucket type
         const bucketProcessor = createBucketProcessor(bucketType, bucketPath);
@@ -119,43 +119,45 @@ export default new Command()
             new: Object.keys(newPayload).length,
             updated: Object.keys(updatedPayload).length,
             deleted: Object.keys(deletedPayload).length,
-            total: Object.keys(processablePayload).length,
+            processable: Object.keys(processablePayload).length,
           };
-          if (flags.frozen && (payloadStats.total > 0 || payloadStats.deleted > 0)) {
+
+          if (flags.frozen && (payloadStats.processable > 0 || payloadStats.deleted > 0)) {
             throw new Error(`Translations are not up to date. Run the command without the --frozen flag to update the translations, then try again.`);
           }
-          if (payloadStats.total === 0) {
+
+          let processedPayload: Record<string, string> = {};
+          if (!payloadStats.processable) {
             localeOra.succeed('Translations are up to date');
-            continue;
+          } else {
+            localeOra.info(`Found ${payloadStats.processable} keys (${payloadStats.new} new, ${payloadStats.updated} updated, ${payloadStats.deleted} deleted)`);
+
+            try {
+              // Use the SDK to localize the payload
+              localeOra.start('AI translation in progress...');
+              processedPayload = await replexicaEngine.localize(
+                processablePayload,
+                {
+                  sourceLocale: i18nConfig.locale.source,
+                  targetLocale: targetLocale,
+                },
+                (progress) => {
+                  localeOra.text = `(${progress}%) AI translation in progress...`;
+                }
+              );
+
+              localeOra.succeed(`AI translation completed`);
+            } catch (error: any) {
+              localeOra.fail(error.message);
+            }
           }
-          localeOra.info(`Found ${payloadStats.total} keys (${payloadStats.new} new, ${payloadStats.updated} updated, ${payloadStats.deleted} deleted)`);
-          
-          try {
-            // Use the SDK to localize the payload
-            localeOra.start('AI translation in progress...');
-            const processedPayload = await replexicaEngine.localize(
-              processablePayload,
-              {
-                sourceLocale: i18nConfig.locale.source,
-                targetLocale: targetLocale,
-              },
-              (progress) => {
-                localeOra.text = `(${progress}%) AI translation in progress...`;
-              }
-            );
-            
-            localeOra.succeed(`AI translation completed`);
-            
-            // Merge the processed payload and the original target payload into a single entity
-            const newTargetPayload = _.omit(
-              _.merge(targetPayload, processedPayload),
-              Object.keys(deletedPayload),
-            );
-            // Save the new target payload
-            await bucketProcessor.save(targetLocale, newTargetPayload);
-          } catch (error: any) {
-            localeOra.fail(error.message);
-          }
+          // Merge the processed payload and the original target payload into a single entity
+          const newTargetPayload = _.omit(
+            _.merge(targetPayload, processedPayload),
+            Object.keys(deletedPayload),
+          );
+          // Save the new target payload
+          await bucketProcessor.save(targetLocale, newTargetPayload);
         }
         // Update the lockfile with the new checksums after the process is done
         bucketOra.start('Updating i18n lockfile');
