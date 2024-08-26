6fdc5d535a077bb0656d37c5edf3423dd32e6412
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-08-26 13:52:31 +0200

    feat(cli): add `jsonrepair` to json loader

---
+++ .changeset/tiny-experts-drop.md
@@ -0,0 +1,6 @@
+---
+"@replexica/cli": patch
+"replexica": patch
+---
+
+Add json repair to json file loader
--- packages/cli/src/workers/bucket/v2/json.ts
+++ packages/cli/src/workers/bucket/v2/json.ts
@@ -1,8 +1,11 @@
+import { jsonrepair } from 'jsonrepair'
+
 import { BucketLoader } from './_base';
 
 export const jsonLoader = (): BucketLoader<string, Record<string, any>> => ({
   async load(text: string) {
-    return JSON.parse(text || '{}');
+    const repaired = jsonrepair(text || '{}');
+    return JSON.parse(repaired);
   },
   async save(payload) {
     return JSON.stringify(payload, null, 2);
