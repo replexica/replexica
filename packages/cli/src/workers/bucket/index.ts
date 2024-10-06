c1b4f71748fdf74029311950f573602c8190731f
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-10-06 13:13:08 -0700

    refactor: remove old code from bucket workers

--- packages/cli/src/workers/bucket/index.ts
+++ packages/cli/src/workers/bucket/index.ts
@@ -1,81 +1,137 @@
-import { localeCodeSchema, bucketTypeSchema } from '@replexica/spec';
+import { bucketTypeSchema } from '@replexica/spec';
 import Z from 'zod';
-import { createFileLoader, IBucketLoader } from './loader';
-import { createAndroidParser, createJsonParser, createMarkdownParser, createXcodeParser, createYamlParser, IBucketParser } from './parser';
-import { BucketPathResolver, patternPathResolver, plainPathResolver } from './path-resolver';
+import path from 'path';
+import * as glob from 'glob';
 
-export function createBucketProcessor(bucketType: Z.infer<typeof bucketTypeSchema>, bucketPath: string) {
-  switch (bucketType) {
-    default:
-      throw new Error(`Unsupported bucket type: ${bucketType}`);
-    case 'json':
-      return composeBucketProcessor(bucketPath, {
-        storage: createFileLoader(),
-        parser: createJsonParser(),
-        pathResolver: patternPathResolver,
-      })
-    case 'yaml':
-      return composeBucketProcessor(bucketPath, {
-        storage: createFileLoader(),
-        parser: createYamlParser({ rootKey: false }),
-        pathResolver: patternPathResolver,
-      });
-    case 'yaml-root-key':
-      return composeBucketProcessor(bucketPath, {
-        storage: createFileLoader(),
-        parser: createYamlParser({ rootKey: true }),
-        pathResolver: patternPathResolver,
-      });
-    case 'markdown':
-      return composeBucketProcessor(bucketPath, {
-        storage: createFileLoader(),
-        parser: createMarkdownParser(),
-        pathResolver: patternPathResolver,
-      });
-    case 'xcode':
-      return composeBucketProcessor(bucketPath, {
-        storage: createFileLoader(),
-        parser: createXcodeParser(),
-        pathResolver: plainPathResolver,
-      });
-    case 'android':
-      return composeBucketProcessor(bucketPath, {
-        storage: createFileLoader(),
-        parser: createAndroidParser(),
-        pathResolver: patternPathResolver,
-      });
-    case 'compiler':
-      return composeBucketProcessor(`node_modules/@replexica/.cache/[locale].json`, {
-        storage: createFileLoader(),
-        parser: createJsonParser(),
-        pathResolver: patternPathResolver,
-      });
+import { composeLoaders } from './_base';
+import { textLoader } from './text-file';
+import { jsonLoader } from './json';
+import { flatLoader } from './flat';
+import { yamlLoader } from './yaml';
+import { rootKeyLoader } from './root-key';
+import { markdownLoader } from './markdown';
+import { xcodeLoader } from './xcode';
+import { androidLoader } from './android';
+import { propertiesLoader } from './properties';
+
+// Path expansion
+export function expandPlaceholderedGlob(pathPattern: string, sourceLocale: string): string[] {
+  // Throw if pathPattern is an absolute path
+  if (path.isAbsolute(pathPattern)) {
+    throw new Error(`Invalid path pattern: ${pathPattern}. Path pattern must be relative.`);
   }
+  // Throw if pathPattern points outside the current working directory
+  if (path.relative(process.cwd(), pathPattern).startsWith('..')) {
+    throw new Error(`Invalid path pattern: ${pathPattern}. Path pattern must be within the current working directory.`);
+  }
+  // Throw error if pathPattern contains "**" – we don't support recursive path patterns
+  if (pathPattern.includes('**')) {
+    throw new Error(`Invalid path pattern: ${pathPattern}. Recursive path patterns are not supported.`);
+  }
+  // Throw error if pathPattern contains "[locale]" several times
+  if (pathPattern.split('[locale]').length > 2) {
+    throw new Error(`Invalid path pattern: ${pathPattern}. Path pattern must contain at most one "[locale]" placeholder.`);
+  }
+  // Break down path pattern into parts
+  const pathPatternChunks = pathPattern.split(path.sep);
+  // Find the index of the segment containing "[locale]"
+  const localeSegmentIndex = pathPatternChunks.findIndex((segment) => segment.includes('[locale]'));
+  // Find the position of the "[locale]" placeholder within the segment
+  const localePlaceholderIndex = pathPatternChunks[localeSegmentIndex]?.indexOf('[locale]') ?? -1;
+  // substitute [locale] in pathPattern with sourceLocale
+  const sourcePathPattern = pathPattern.replace(/\[locale\]/g, sourceLocale);
+  // get all files that match the sourcePathPattern
+  const sourcePaths = glob
+    .sync(sourcePathPattern, { follow: true, withFileTypes: true })
+    .filter((file) => file.isFile())
+    .map((file) => file.fullpath())
+    .map((fullpath) => path.relative(process.cwd(), fullpath));
+  // transform each source file path back to [locale] placeholder paths
+  const placeholderedPaths = sourcePaths.map((sourcePath) => {
+    const sourcePathChunks = sourcePath.split(path.sep);
+    if (localeSegmentIndex >= 0 && localePlaceholderIndex >= 0) {
+      const placeholderedPathChunk = sourcePathChunks[localeSegmentIndex];
+      const placeholderedSegment =
+        placeholderedPathChunk.substring(0, localePlaceholderIndex)
+        + '[locale]'
+        + placeholderedPathChunk.substring(localePlaceholderIndex + sourceLocale.length)
+      ;
+      sourcePathChunks[localeSegmentIndex] = placeholderedSegment;
+    }
+    const placeholderedPath = sourcePathChunks.join(path.sep);
+    return placeholderedPath;
+  });
+  // return the placeholdered paths
+  return placeholderedPaths;
 }
 
-// Helpers
-
-type BucketProcessorParams = {
-  storage: IBucketLoader;
-  parser: IBucketParser;
-  pathResolver: BucketPathResolver;
+export type CreateBucketLoaderParams = {
+  bucketType: Z.infer<typeof bucketTypeSchema>;
+  placeholderedPath: string;
+  locale: string;
 };
-
-function composeBucketProcessor(bucketPath: string, params: BucketProcessorParams) {
-  return {
-    async load(locale: Z.infer<typeof localeCodeSchema>) {
-      const filePath = await params.pathResolver(locale, bucketPath);
-      const content = await params.storage.load(locale, filePath);
-      if (!content) { return {}; }
-
-      const payload = await params.parser.deserialize(locale, content);
-      return payload;
-    },
-    async save(locale: Z.infer<typeof localeCodeSchema>, payload: Record<string, string>) {
-      const serialized = await params.parser.serialize(locale, payload);
-
-      const filePath = await params.pathResolver(locale, bucketPath);
-      await params.storage.save(locale, filePath, serialized);
-    },
-  };
+export function createBucketLoader(params: CreateBucketLoaderParams) {
+  const filepath = params.placeholderedPath.replace(/\[locale\]/g, params.locale);
+  switch (params.bucketType) {
+    default:
+      throw new Error(`Unsupported bucket type: ${params.bucketType}`);
+    case 'json':
+      return composeLoaders<string, Record<string, string>>(
+        textLoader(filepath),
+        jsonLoader(),
+        flatLoader(),
+      );
+    case 'yaml':
+      return composeLoaders<string, Record<string, string>>(
+        textLoader(filepath),
+        yamlLoader(),
+        flatLoader(),
+      );
+    case 'markdown':
+      return composeLoaders<string, Record<string, string>>(
+        textLoader(filepath),
+        markdownLoader(),
+        flatLoader(),
+      );
+    case 'xcode':
+      return composeLoaders<string, Record<string, string>>(
+        xcodeLoader(
+          params.locale,
+          composeLoaders<void, Record<string, any>>(
+            textLoader(filepath),
+            jsonLoader(),
+          ),
+        ),
+        flatLoader(),
+      );
+    case 'yaml-root-key':
+      return composeLoaders<string, Record<string, string>>(
+        rootKeyLoader(
+          params.locale,
+          composeLoaders(
+            textLoader(filepath),
+            yamlLoader(),
+          ),
+        ),
+        flatLoader(),
+      );
+    case 'android':
+      return composeLoaders<string, Record<string, string>>(
+        textLoader(filepath),
+        androidLoader(),
+        flatLoader(),
+      );
+    case 'compiler':
+      return composeLoaders<string, Record<string, string>>(
+        textLoader('node_modules/@replexica/.cache/[locale].json'),
+        jsonLoader(),
+        flatLoader(),
+      );
+    case 'properties':
+      return composeLoaders<string, Record<string, string>>(
+        textLoader(filepath),
+        propertiesLoader(),
+        flatLoader(),
+      );
+  }
 }
