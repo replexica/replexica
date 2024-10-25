1cc07961d221e397ad5dd2917bed76cb4f2b1f04
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-10-25 10:38:14 -0700

    fix(cli): add path resolve to text loaders

---
+++ .changeset/red-cows-end.md
@@ -0,0 +1,6 @@
+---
+"@replexica/cli": patch
+"replexica": patch
+---
+
+add path.resolve to text loaders
--- packages/cli/src/workers/bucket/text-file.ts
+++ packages/cli/src/workers/bucket/text-file.ts
@@ -1,18 +1,23 @@
 import fs from 'fs/promises';
+import path from 'path'; // Import the path module
 import { BucketLoader } from './_base';
 
-export const textLoader = (filePath: string): BucketLoader<void, string> => ({
-  async load() {
-    const exists = await fs.access(filePath).then(() => true).catch(() => false);
-    if (!exists) { return ''; }
+export const textLoader = (filePath: string): BucketLoader<void, string> => {
+  const resolvedPath = path.resolve(filePath); // Resolve the file path
 
-    return await fs.readFile(filePath, 'utf-8');
-  },
-  async save(payload: string) {    
-    // create parent directories if they don't exist
-    await fs.mkdir(filePath.split('/').slice(0, -1).join('/'), { recursive: true });
-    // write the file
-    const finalPayload = payload.trim() + '\n';
-    await fs.writeFile(filePath, finalPayload, { encoding: 'utf-8', flag: 'w' });
-  },
-})
+  return {
+    async load() {
+      const exists = await fs.access(resolvedPath).then(() => true).catch(() => false);
+      if (!exists) { return ''; }
+
+      return await fs.readFile(resolvedPath, 'utf-8');
+    },
+    async save(payload: string) {    
+      // create parent directories if they don't exist
+      await fs.mkdir(resolvedPath.split('/').slice(0, -1).join('/'), { recursive: true });
+      // write the file
+      const finalPayload = payload.trim() + '\n';
+      await fs.writeFile(resolvedPath, finalPayload, { encoding: 'utf-8', flag: 'w' });
+    },
+  };
+}
