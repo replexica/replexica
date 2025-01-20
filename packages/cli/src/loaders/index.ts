8db4527d9c3501d97f8bb7b414dd61e8a3ee80f6
Max Prilutskiy <5614659+maxprilutskiy@users.noreply.github.com>
2025-01-20 18:16:37 +0100

    feat(dato): blocks (#387)
    * chore: datocms drafts
    
    * feat: add dato single blocks support
    
    * chore: draft
    
    * feat: blocks
    
    * chore: changeset

--- packages/cli/src/loaders/index.ts
+++ packages/cli/src/loaders/index.ts
@@ -150,6 +150,7 @@ export default function createBucketLoader(
     case "dato":
       return composeLoaders(
         createDatoLoader(bucketPathPattern),
+        createFlatLoader(),
         createUnlocalizableLoader(),
       );
   }
